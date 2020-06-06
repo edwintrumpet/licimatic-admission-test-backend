2ff39074265f6acbf7d02e9c56133c7449cc38db
Edwin García <spark.com.co@gmail.com>
2020-06-06 18:50:04 -0500

    Set tests with Jest

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const debug = require('debug')('app:server');
 const { port } = require('./config/index');
+const routes = require('./routes');
 
+// Initializations
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+// Routes
+routes(app);
 
+// Server
 app.listen(port, (err) => {
   if (err) debug(err);
   else debug(`Listening on http://localhost:${port}`);
